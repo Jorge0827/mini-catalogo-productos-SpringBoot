f2293405e0aaa3ce5a5701957658645471f9bf4d
Jorge Andres Echavarria Pardo <jorgean0827@gmail.com>
2025-05-05 17:43:07 -0500

    Finalización de los endpoints en el controlador

--- src/main/java/com/jechavarria/catalogo/mini_catalogo_productos/controller/ProductController.java
+++ src/main/java/com/jechavarria/catalogo/mini_catalogo_productos/controller/ProductController.java
@@ -3,7 +3,12 @@ package com.jechavarria.catalogo.mini_catalogo_productos.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,9 +35,38 @@ public class ProductController {
 
     }
 
-    @GetMapping 
+    @GetMapping // Método GET para obtener todos los productos
     public List<ProductDto> getAllProducts() {
         return products;
     }
 
+    @GetMapping("/{id}") // Método GET para obtener un producto por su ID
+    public ProductDto getProductById(@PathVariable Long id) {
+        for (ProductDto productDto : products) {
+            if (productDto.getId().equals(id)) {
+                return productDto;
+            }
+
+        }
+        return null;
+
+    }
+
+    @PostMapping // Método POST para crear un nuevo producto
+    public ProductDto createProduct(@RequestBody ProductDto newProductDto) { // @RequestBody indica que el cuerpo de la petición se convertirá en un objeto ProductDto
+        products.add(newProductDto);
+        return newProductDto;
+    }
+
+    @DeleteMapping("/{id}") // Método DELETE para eliminar un producto por su ID
+    public void deleteProduct(@PathVariable Long id) {
+        products.removeIf(productDto -> productDto.getId().equals(id));
+    }
+
+    @PutMapping("/{id}") // Método PUT para actualizar un producto por su ID
+    public ProductDto updateProduct(@PathVariable int id, @RequestBody ProductDto updatedProductDto) {
+        products.set(id, updatedProductDto);
+        return updatedProductDto;
+
+    }
 }
