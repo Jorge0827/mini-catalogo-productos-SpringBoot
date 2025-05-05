235beac1e1da06e5b2412c0545c22fe9c2391941
Jorge Andres Echavarria Pardo <jorgean0827@gmail.com>
2025-05-05 16:58:15 -0500

    Proyecto base y estructura de paquetes creada

---
+++ src/main/java/com/jechavarria/catalogo/mini_catalogo_productos/MiniCatalogoProductosApplication.java
@@ -0,0 +1,13 @@
+package com.jechavarria.catalogo.mini_catalogo_productos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniCatalogoProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniCatalogoProductosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jechavarria/catalogo/mini_catalogo_productos/MiniCatalogoProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jechavarria.catalogo.mini_catalogo_productos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniCatalogoProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
